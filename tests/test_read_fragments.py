efae29e6c397a294c2f354ec033ab596f8bf8615
SqFKYo <sqfkyo@iki.fi>
2023-01-19 19:14:00 +0200

    First step done for fragment checking.

--- tests/test_read_fragments.py
+++ tests/test_read_fragments.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
-from pytest_bdd import scenarios
+from pytest_bdd import parsers, scenarios, then
 
 scenarios("fragment_reading.feature")
+
+@then(parsers.parse("we have Fragment with obj_id of {obj_id}"))
+def assert_fragment_exists(context, obj_id):
+    assert obj_id in context.converter.fragments
